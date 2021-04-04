c4d8888635d106e27b0977c786a8ecaff94a8577
Alex <dinath.contact@gmail.com>
2021-04-04 17:30:50 +0200

    🔊 Display PHP errors

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 use Symfony\Component\Dotenv\Dotenv;
 
 require __DIR__ . '/vendor/autoload.php';
